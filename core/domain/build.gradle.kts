c484df5a9b42a6aa908d8ae3d48ef62ac5a1d7e8
kkh <kkhyungyung0@naver.com>
2025-07-18 21:28:47 +0900

    chore : java version

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -3,16 +3,12 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
-    jvmToolchain(11)
+    jvmToolchain(17)
 }
-
 
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
