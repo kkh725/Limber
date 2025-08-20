2987471733da67576b186d3b34fdf450074f7a43
kkh <kkhyungyung0@naver.com>
2025-08-20 20:52:22 +0900

    refactor : RetrospectsApi.kt 리펙토링

--- core/network/src/main/java/com/kkh/multimodule/core/network/api/timer/RetrospectsApi.kt
+++ core/network/src/main/java/com/kkh/multimodule/core/network/api/timer/RetrospectsApi.kt
@@ -3,6 +3,7 @@ package com.kkh.multimodule.core.network.api.timer
 import com.kkh.multimodule.core.network.model.request.RetrospectsRequestDto
 import com.kkh.multimodule.core.network.model.response.BaseResponse
 import com.kkh.multimodule.core.network.model.response.SingleTimerResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.POST
@@ -15,7 +16,7 @@ interface RetrospectsApi {
     @POST("/api/timer-retrospects")
     suspend fun writeRetrospects(
         @Body retrospectsDto : RetrospectsRequestDto
-    ): BaseResponse
+    ): Response<BaseResponse>
 
     /**
      * 타이머 회고 삭제
@@ -23,6 +24,6 @@ interface RetrospectsApi {
     @DELETE("/api/timer-retrospects/{timerRetrospectsId}")
     suspend fun deleteRetrospects(
         @Path ("timerRetrospectsId") timerRetrospectsId: Int
-    ): BaseResponse
+    ): Response<BaseResponse>
 
 }
